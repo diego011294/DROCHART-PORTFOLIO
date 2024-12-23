4641db183f35ca54ad9818ca37036a8db90be24d
diego <diego0112@hotmail.com>
2024-12-23 23:52:25 +0100

    Cambios en la pantalla de carga, añadido vercel.json y etiquetas opengraph

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,45 @@ import { Dobleuve } from './pages/Dobleuve'
 import { ScrollToTop } from './context/ScrollToTop'
 import { WorkActive } from './components/WorkActive'
 import { LoadingScreen } from './components/LoadingScreen'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { DrochGaleria } from './pages/DrochGaleria'
 import { DrochPortfolio } from './pages/DrochPortfolio'
 
-
 function App() {
   const [loadingComplete, setLoadingComplete] = useState(false);
 
+  useEffect(() => {
+    // Revisa si ya se mostró el LoadingScreen previamente
+    const hasSeenLoading = localStorage.getItem('hasSeenLoading');
+
+    if (hasSeenLoading) {
+      setLoadingComplete(true); // Si ya se mostró, no vuelve a aparecer
+    } else {
+      // Si no se ha mostrado, muestra el LoadingScreen
+      setTimeout(() => {
+        setLoadingComplete(true);
+        localStorage.setItem('hasSeenLoading', 'true'); // Guarda el estado en localStorage
+      }, 3000); // Simula una duración de 3 segundos en la pantalla de carga
+    }
+  }, []);
+
+  if (!loadingComplete) {
+    return <LoadingScreen />;
+  }
+
   return (
-    <div className="flex flex-col min-h-screen" >
-      {loadingComplete &&(<motion.div
-      initial={{ y: -100, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }} 
-      transition={{ duration: 0.6, ease: "easeOut" }}
-      className='sticky top-0 z-50' 
+    <div className="flex flex-col min-h-screen">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+        className="sticky top-0 z-50"
       >
-         <Navbar />
-         </motion.div>) }
-      {loadingComplete && <WorkActive />}
+        <Navbar />
+      </motion.div>
+      <WorkActive />
       <ScrollToTop />
-      {loadingComplete ? (
       <div className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -37,13 +54,10 @@ function App() {
           <Route path="/drochportfolio" element={<DrochPortfolio />} />
         </Routes>
       </div>
-       ) : (
-        <LoadingScreen setLoadingComplete={setLoadingComplete} />
-      )}
-      {loadingComplete && <Footer />}
-      {loadingComplete && <BotFooter />}
+      <Footer />
+      <BotFooter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
